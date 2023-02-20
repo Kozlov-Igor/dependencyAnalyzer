0fa202671cd93bf72a510f76c819e889b521db38
Igor Kozlov <kozlovigor1989@gmail.com>
2023-02-20 22:20:17 +0100

    Changed status code 200 in response.

--- server/src/analyze/analyze.controller.ts
+++ server/src/analyze/analyze.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, HttpCode } from '@nestjs/common';
 import { AnalyzeService } from './analyze.service';
 import { AnalyzeDto } from './dto/analyze.dto';
 
@@ -7,6 +7,7 @@ export class AnalyzeController {
   constructor(private readonly analyzeService: AnalyzeService) {}
 
   @Post('framework')
+  @HttpCode(200)
   create(@Body() analyzeDto: AnalyzeDto) {
     return this.analyzeService.analyzeFramework(analyzeDto);
   }

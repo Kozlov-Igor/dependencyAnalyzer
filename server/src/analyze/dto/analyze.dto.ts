b491c6c0104d0042fb202e71ef373bde889a0022
Igor Kozlov <kozlovigor1989@gmail.com>
2023-02-20 22:18:51 +0100

    Added ProjectType in API request

--- server/src/analyze/dto/analyze.dto.ts
+++ server/src/analyze/dto/analyze.dto.ts
@@ -4,10 +4,21 @@ export enum SourceType {
     GitHub = "GitHub"
 }
 
+export enum ProjectType {
+  IOS = "iOS",
+  DOTNET = ".NET",
+  JAVA = "Java",
+  FLUTTER = "FLUTTER"
+}
+
 export class AnalyzeDto {
   @IsNotEmpty()
   @IsEnum(SourceType)
   SourceType: SourceType;
+
+  @IsNotEmpty()
+  @IsEnum(ProjectType)
+  ProjectType: ProjectType;
 
   @IsNotEmpty()
   @IsUrl()
